3d0de8e48ae4a18acad76bf4a5a87b1d22e35801
Ven Popov <vencislav.popov@gmail.com>
2025-02-16 18:37:10 +0100

    simple coin flip program to practice std::generate from algorithm

---
+++ src/03-ranges-generate.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <random>
+
+auto coin() {
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  static std::uniform_int_distribution<int> dist{0, 1};
+  return dist(gen);
+}
+
+int main() {
+  std::cout << "How many flips?\n";
+
+  int n;
+  std::cin >> n; // ignoring input validation for now
+
+  std::vector<int> flips(n);
+  std::generate(flips.begin(), flips.end(), coin);
+
+  for (auto item : flips) {
+    std::cout << item << ' ';
+  }
+  std::cout << '\n';
+  return 0;
+}
